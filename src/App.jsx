24e6bc81d3d3ed20c1852cd820bd4eda925758cf
maufdev09 <marufdev09@gmail.com>
2023-06-07 11:59:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold underline">Hello world!</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+    </div>
+  );
+};
+
+export default App;
