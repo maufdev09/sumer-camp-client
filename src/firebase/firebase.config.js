9a2f71750712a11eaad82f6a3a3b92064cf7d597
maufdev09 <marufdev09@gmail.com>
2023-06-07 16:45:03 +0600

    firebase config setup and key in env setup done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAlqfem3ewuPTZyE1y480rrCEJRS5LO4tg",
-  authDomain: "sports-pro-96acb.firebaseapp.com",
-  projectId: "sports-pro-96acb",
-  storageBucket: "sports-pro-96acb.appspot.com",
-  messagingSenderId: "454720356939",
-  appId: "1:454720356939:web:5fdb6e44d666bfbb1c83ce",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
